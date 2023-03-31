0bc536a5f7409d366a26495809583892fd269482
Doherty <Devin.Doherty@stvinc.com>
2023-03-31 01:21:36 -0400

    Add player class and draw_map, draw_player

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ MAP = [
     [0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+class Player:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 
 state = {
@@ -23,6 +29,29 @@ clock = pygame.time.Clock()
 running = True
 
 wall_types = pygame.image.load("assets/monochrome_wall.png").convert_alpha()
+
+player = Player(6, 6)
+
+def draw_map():
+    x = 0
+    y = 0
+    for row in MAP:
+        for column in row:
+            if column == 1:
+                pygame.draw.rect(screen, "red", (x, y, 5, 5))
+                x += 6
+                # print(f"Red tile drawn at ({x}, {y})")
+            if column == 0:
+                pygame.draw.rect(screen, "blue", (x, y, 5, 5))
+                x += 6
+                # print(f"Blue tile drawn at ({x}, {y})")
+        x = 0
+        y += 6
+
+def draw_player():
+    pygame.draw.rect(screen, "green", (player.x, player.y, 5, 5))
+    pygame.draw.line(screen, "green", (player.x, player.y), (player.x, player.y - 3))
+
 
 def input(state, screen):
     print("Input")
@@ -37,6 +66,8 @@ def render(screen):
     print("Render")
     screen.fill("purple")
     screen.blit(wall_types,(0, 0))
+    draw_map()
+    draw_player()
 
     pygame.display.flip()
 
